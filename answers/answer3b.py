5303677a8c2f32013ac7e1118aa9775c7fd7db07
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2019-05-03 21:52:29 +0100

    Create answer3b.py

---
+++ answers/answer3b.py
@@ -0,0 +1,6 @@
+
+bystreet = street.groupby(['Location','Crime type']).count()
+bystreet = bystreet.drop(columns=['Month', 'Last outcome category','coordinates','LSOA code'])
+bystreet = bystreet.rename(index=str, columns={"Crime ID": "Number of crimes"})
+
+bystreet.sort_values(by=['Number of crimes'], ascending=False).head()
