100e76227def928db612c302d20b9780f59c5d89
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2019-05-06 13:12:06 +0100

    Create answer8.py

---
+++ answers/answer8.py
@@ -0,0 +1,8 @@
+
+fig, axs = plt.subplots(1, 2, figsize=(20,5))
+
+p1=boroughs4.plot(column='Controlled drugs',ax=axs[0],cmap='Blues',legend=True);
+p2=boroughs4.plot(column='Stolen goods',ax=axs[1], cmap='Reds',legend=True);
+
+axs[0].set_title('Controlled drugs', fontdict={'fontsize': '12', 'fontweight' : '5'});
+axs[1].set_title('Stolen goods', fontdict={'fontsize': '12', 'fontweight' : '5'});
