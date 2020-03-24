41e743c4cb42ba46f748a8c1c1fb9d42f8c46eaf
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-24 21:10:52 +0000

    Update answer6.py

--- answers/answer6.py
+++ answers/answer6.py
@@ -1,8 +1,6 @@
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,5))
-
-stop_search['Object of search'].groupby(stop_search['Object of search']).count().plot.bar(ax=ax1);
-
-stop_search['Outcome'].groupby(stop_search['Outcome']).count().plot.bar(ax=ax2);
-
-stop_search['Month'] = stop_search.Date.dt.to_period("M")
-stop_search['Object of search'].groupby(stop_search['Month']).count().plot(ax=ax3);
+dfsjoin2 = gpd.sjoin(boroughs,stop_search[stop_search['Outcome'] == 'Arrest']) 
+dfpivot2 = pd.pivot_table(dfsjoin2,index='id',columns='Object of search',aggfunc={'Object of search':'count'})
+dfpivot2.columns = dfpivot2.columns.droplevel()
+dfpivot2 = dfpivot2.reset_index()
+boroughs4 = boroughs.merge(dfpivot2, how='left',on='id')
+boroughs4.head()
