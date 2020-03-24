1cb75c822dd3898b492c87e0f5b4bedfffe7ffa3
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-24 20:55:41 +0000

    Update answer5.py

--- answers/answer5.py
+++ answers/answer5.py
@@ -1,5 +1,8 @@
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,5))
 
-street['Crime type'].groupby(street['Crime type']).count().plot.bar(ax=ax1);
+stop_search['Object of search'].groupby(stop_search['Object of search']).count().plot.bar(ax=ax1);
 
-street['Crime type'].groupby(street['Month']).count().plot(ax=ax2);
+stop_search['Outcome'].groupby(stop_search['Outcome']).count().plot.bar(ax=ax2);
+
+stop_search['Month'] = stop_search.Date.dt.to_period("M")
+stop_search['Object of search'].groupby(stop_search['Month']).count().plot(ax=ax3);
