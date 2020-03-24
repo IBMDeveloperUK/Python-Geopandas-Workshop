b7720398bd82f9e193c740f87f654677053e191f
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-24 20:54:28 +0000

    Update answer4.py

--- answers/answer4.py
+++ answers/answer4.py
@@ -1,20 +1,5 @@
-# number of data points
-print ('rows in stop_search: '+str(len(stop_search)))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
 
-# columns 
-print ('Columns: '+str(stop_search.columns))
+street['Crime type'].groupby(street['Crime type']).count().plot.bar(ax=ax1);
 
-# categories
-print ('Policing operation: '+str(stop_search['Policing operation'].unique()))
-print ('Legislation: '+str(stop_search['Legislation'].unique()))
-print ('Object of search: '+str(stop_search['Object of search'].unique()))
-print ('Outcome: '+str(stop_search['Outcome'].unique()))
-print ('Outcome linked to object of search: '+str(stop_search['Outcome linked to object of search'].unique()))
-
-# delete columns
-stop_search = stop_search.drop(columns=['Unnamed: 0','Latitude', 'Longitude', 'Part of a policing operation', 'Policing operation', 'Outcome linked to object of search'])
-
-# convert Date to datetime
-stop_search['Date'] = stop_search['Date'].apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S+00:00"))
-
-stop_search.head()
+street['Crime type'].groupby(street['Month']).count().plot(ax=ax2);
