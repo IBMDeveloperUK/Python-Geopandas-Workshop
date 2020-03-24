24cf6d234e29f85c6cc89267a3af963860b478f5
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2020-03-24 20:48:42 +0000

    Update answer3.py

--- answers/answer3.py
+++ answers/answer3.py
@@ -1,18 +1,20 @@
 # number of data points
-print ('rows in street: '+str(len(street)))
+print ('rows in stop_search: '+str(len(stop_search)))
 
 # columns 
-print ('Columns: '+str(street.columns))
+print ('Columns: '+str(stop_search.columns))
 
 # categories
-print ('Crime type: '+str(street['Crime type'].unique()))
-print ('Last outcome category: '+str(street['Last outcome category'].unique()))
-print (street['Context'].unique())
+print ('Policing operation: '+str(stop_search['Policing operation'].unique()))
+print ('Legislation: '+str(stop_search['Legislation'].unique()))
+print ('Object of search: '+str(stop_search['Object of search'].unique()))
+print ('Outcome: '+str(stop_search['Outcome'].unique()))
+print ('Outcome linked to object of search: '+str(stop_search['Outcome linked to object of search'].unique()))
 
 # delete columns
-street = street.drop(columns=['Unnamed: 0','Latitude', 'Longitude','Context'])
+stop_search = stop_search.drop(columns=['Unnamed: 0','Latitude', 'Longitude', 'Part of a policing operation', 'Policing operation', 'Outcome linked to object of search'])
 
 # convert Date to datetime
-street['Month'] = street['Month'].apply(lambda x: datetime.strptime(x, "%Y-%m"))
+stop_search['Date'] = stop_search['Date'].apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S+00:00"))
 
-street.head()
+stop_search.head()
